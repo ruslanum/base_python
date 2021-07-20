520d1e73af0a5ab95e9cf9af3643a12b65192421
ruslanum <86016842+ruslanum@users.noreply.github.com>
2021-07-20 22:36:52 +0300

    Update car.py

--- homework_02/car.py
+++ homework_02/car.py
@@ -12,7 +12,7 @@ class Car(Vehicle):
         super(Car, self).__init__(weight, fuel, fuel_consumption)
 
     def set_engine(self):
-        self.engine = Engine()
+        self.engine = Engine
         return self.engine
 
     def __str__(self):
