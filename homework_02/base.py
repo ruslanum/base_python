2c0b4ccf91db65eb1b0f527b96078eb30e78b399
ruslanum <86016842+ruslanum@users.noreply.github.com>
2021-07-21 20:15:45 +0300

    Update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -21,7 +21,7 @@ class Vehicle:
 
     def move(self, weight):
         self.weight = weight
-        if self.started is True:
+        if Vehicle.started is True:
             if self.weight * self.fuel_consumption <= self.fuel != 0:
                 self.fuel = self.fuel - self.weight * self.fuel_consumption
                 return self.fuel
